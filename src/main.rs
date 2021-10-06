89c7046d79d9b91e77f31e4f548e7bd0dc1bda43
Yehonatan Bitton <yehonatanb@sentinelone.com>
2021-10-07 01:29:32 +0300

    graph_parser: create single graph for all data (use one file instead of multiple dot files)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use structopt::StructOpt;
 use std::collections::HashMap;
 use petgraph::Graph;
-use petgraph::graph::NodeIndex;
 use petgraph::dot::{Dot, Config};
 use std::fs::File;
 use std::io::Write;
@@ -38,24 +37,46 @@ fn parse_line(line: &str, key_del: &str, data_del: &str) -> HashMap<String, Stri
             Some((key, value)) => {
                 res.insert(key.trim().to_string(), value.trim().to_string());
             },
-            None => println!("No key found")
+            None => println!("No key found: {}", data)
         }
     }
     return res;
 }
 
-fn parse_log(content: String, pattern: String, key_del: String, data_del: String, group_by: &String, label_keys: &Vec<String>) -> HashMap<String, Graph::<String, i32>>{
-    let mut log_map: HashMap<String, Graph::<String, i32>> = HashMap::new();
-    let mut latest_nodes: HashMap<String, NodeIndex> = HashMap::new();
+fn log_to_map(content: String, pattern: String, key_del: String, data_del: String, group_by: &String) -> HashMap<String, Vec<HashMap<String, String>>>{
+    let mut log_map:HashMap<String, Vec<HashMap<String, String>>> = HashMap::new();
     for line in content.lines() {
         if line.contains(&pattern) {
             let splitted_line: Vec<&str> = line.split(&pattern).collect();
             let parsed_line = parse_line(splitted_line[1], &key_del, &data_del);
-            let group_key_value = parsed_line.get(group_by).unwrap().to_string();
-            let group_graph = log_map.get_mut(&group_key_value);
+            match parsed_line.get(group_by) {
+                Some(group_key_value) => {
+                    match log_map.get_mut(group_key_value) {
+                        Some(events) => {
+                            events.push(parsed_line);
+                        },
+                        None => {
+                            let mut events = Vec::new();
+                            events.push(parsed_line.to_owned());
+                            log_map.insert(group_key_value.to_string(), events);
+                        }
+                    }
+                },
+                None => println!("no group key {}", group_by)
+            }
+        }
+    }
+    return log_map;
+}
+
+fn map_to_graph(log_map: HashMap<String, Vec<HashMap<String, String>>>, keys_to_print: &Vec<String>) -> Graph<String, i32> {
+    let mut log_graph = Graph::new();
+    for (group_key, group_events) in log_map {
+        let mut previous_event = log_graph.add_node(format!("start, {}", group_key));
+        for event in group_events {
             let mut label = "".to_owned();
-            for key in label_keys {
-                let parsed_key = parsed_line.get(key);
+            for key in keys_to_print {
+                let parsed_key = event.get(key);
                 match  parsed_key {
                     Some(value) => {
                         label.push_str(format!("{}: {}", key, value).as_str());
@@ -64,39 +85,38 @@ fn parse_log(content: String, pattern: String, key_del: String, data_del: String
                     None => continue
                 }
             }
-            match group_graph {
-                Some(events) => {
-                    let node = events.add_node(label);
-                    let last_node = latest_nodes.get(&group_key_value).unwrap();
-                    events.add_edge(*last_node, node, 0);
-                    latest_nodes.insert(group_key_value, node);
-                },
-                None => {
-                    let mut events = Graph::<String, i32>::new();
-                    let node = events.add_node(label);
-                    log_map.insert(group_key_value.to_string(), events);
-                    latest_nodes.insert(group_key_value, node);
-                }
-            }
+            let current_event = log_graph.add_node(label);
+            log_graph.add_edge(previous_event, current_event, 0);
+            previous_event = current_event;
         }
     }
-    return log_map;
+    return log_graph;
+}
+
+fn parse_log(content: String, pattern: String, key_del: String, data_del: String, group_by: &String, label_keys: &Vec<String>) {
+    let log_map = log_to_map(
+        content,
+        pattern,
+        key_del,
+        data_del,
+        group_by,
+    );
+
+    let log_graph = map_to_graph(log_map, label_keys);
+    let output_dir = std::path::Path::new("./graph_results");
+    std::fs::create_dir_all(output_dir);
+    println!("saving graph!");
+    println!("{:?}", Dot::with_config(&log_graph, &[Config::EdgeNoLabel]));
+    let mut graph_file = File::create(
+        output_dir.join("log_graph.dot")
+    ).unwrap();
+    let output = format!("{}", Dot::with_config(&log_graph, &[Config::EdgeNoLabel]));
+    graph_file.write_all(&output.as_bytes()).expect("could not write file");
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts = Cli::from_args();
     let content = std::fs::read_to_string(opts.path)?;
-    let parsed_log = parse_log(content, opts.pattern, opts.keys_delimiter,  opts.params_delimiter, &opts.group_by, &opts.keys_to_print);
-    let output_dir = std::path::Path::new("./graph_results");
-    std::fs::create_dir_all(output_dir)?;
-    for (group, group_graph) in parsed_log {
-        println!("saving graph! {}", group);
-        println!("{:?}", Dot::with_config(&group_graph, &[Config::EdgeNoLabel]));
-        let mut graph_file = File::create(
-            output_dir.join(format!("{}.dot", group))
-        )?;
-        let output = format!("{}", Dot::with_config(&group_graph, &[Config::EdgeNoLabel]));
-        graph_file.write_all(&output.as_bytes()).expect("could not write file");
-    }
+    parse_log(content, opts.pattern, opts.keys_delimiter,  opts.params_delimiter, &opts.group_by, &opts.keys_to_print);
     Ok(())
 }
