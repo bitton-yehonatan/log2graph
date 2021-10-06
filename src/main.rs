09ff738929da03786059f0567ce2fd6eab38b804
Yehonatan Bitton <yehonatanb@sentinelone.com>
2021-10-07 02:05:13 +0300

    log_parser: support split_files option for splitting dot file accross group key

--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,12 @@ struct Cli {
     /// The key for grouping logs
     #[structopt(short = "g", long = "group_by")]
     group_by: String,
-    /// The key for grouping logs
+    /// Keys for node labling
     #[structopt(short = "v", long = "keys_to_print")]
     keys_to_print: Vec<String>,
+    /// split dot file by group
+    #[structopt(short = "s", long = "split_files")]
+    split_files: bool,
     /// The path to the file to read
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
@@ -69,7 +72,8 @@ fn log_to_map(content: String, pattern: String, key_del: String, data_del: Strin
     return log_map;
 }
 
-fn map_to_graph(log_map: HashMap<String, Vec<HashMap<String, String>>>, keys_to_print: &Vec<String>) -> Graph<String, i32> {
+fn map_to_graph(log_map: HashMap<String, Vec<HashMap<String, String>>>, keys_to_print: &Vec<String>, split_files: bool) -> HashMap<String, Graph<String, i32>> {
+    let mut log_graphs = HashMap::new();
     let mut log_graph = Graph::new();
     for (group_key, group_events) in log_map {
         let mut previous_event = log_graph.add_node(format!("start, {}", group_key));
@@ -89,11 +93,18 @@ fn map_to_graph(log_map: HashMap<String, Vec<HashMap<String, String>>>, keys_to_
             log_graph.add_edge(previous_event, current_event, 0);
             previous_event = current_event;
         }
+        if split_files {
+            log_graphs.insert(group_key, log_graph);
+            log_graph = Graph::new();
+        }
     }
-    return log_graph;
+    if !split_files {
+        log_graphs.insert("graph_result".to_owned(), log_graph);
+    }
+    return log_graphs;
 }
 
-fn parse_log(content: String, pattern: String, key_del: String, data_del: String, group_by: &String, label_keys: &Vec<String>) {
+fn parse_log(content: String, pattern: String, key_del: String, data_del: String, group_by: &String, label_keys: &Vec<String>, split_files: bool) {
     let log_map = log_to_map(
         content,
         pattern,
@@ -102,21 +113,23 @@ fn parse_log(content: String, pattern: String, key_del: String, data_del: String
         group_by,
     );
 
-    let log_graph = map_to_graph(log_map, label_keys);
+    let log_graphs = map_to_graph(log_map, label_keys, split_files);
     let output_dir = std::path::Path::new("./graph_results");
     std::fs::create_dir_all(output_dir);
-    println!("saving graph!");
-    println!("{:?}", Dot::with_config(&log_graph, &[Config::EdgeNoLabel]));
-    let mut graph_file = File::create(
-        output_dir.join("log_graph.dot")
-    ).unwrap();
-    let output = format!("{}", Dot::with_config(&log_graph, &[Config::EdgeNoLabel]));
-    graph_file.write_all(&output.as_bytes()).expect("could not write file");
+    for (group_key, log_graph) in log_graphs {
+        println!("saving graph!");
+        println!("{:?}", Dot::with_config(&log_graph, &[Config::EdgeNoLabel]));
+        let mut graph_file = File::create(
+            output_dir.join(format!("{}.dot", group_key))
+        ).unwrap();
+        let output = format!("{}", Dot::with_config(&log_graph, &[Config::EdgeNoLabel]));
+        graph_file.write_all(&output.as_bytes()).expect("could not write file");
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts = Cli::from_args();
     let content = std::fs::read_to_string(opts.path)?;
-    parse_log(content, opts.pattern, opts.keys_delimiter,  opts.params_delimiter, &opts.group_by, &opts.keys_to_print);
+    parse_log(content, opts.pattern, opts.keys_delimiter,  opts.params_delimiter, &opts.group_by, &opts.keys_to_print, opts.split_files);
     Ok(())
 }
